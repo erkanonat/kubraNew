7bc06248e9b3664cd2070e895f63b8e1a4376af6
ERKAN ONAT <erkanonat90@gmail.com>
2019-09-07 08:46:03 +0300

    last commit

--- src/resources/create_table_kubra.sql
+++ src/resources/create_table_kubra.sql
@@ -1,84 +1,76 @@
--- noinspection SqlNoDataSourceInspectionForFile
 
--- noinspection SqlDialectInspectionForFile
-
-CREATE TABLE sys.`product_details` (
+CREATE TABLE `criteria` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) ,
-  `criteria_id` int(11) ,
-  `range_id` int(11) ,
-    `weight` double(10,2),
+  `issue_id` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `weight` double(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE sys.`product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,  
-  `name` varchar(100) ,
-  `grade` double(10,2),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-ALTER TABLE sys.product  ADD COLUMN price double(10,2) DEFAULT 0
-ALTER TABLE sys.product  ADD COLUMN selected INT DEFAULT 0
-ALTER TABLE sys.product  ADD COLUMN issue_id INT DEFAULT 0
 
-
-CREATE TABLE sys.`range` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `criteria_id` int(11) ,
-  `range_name` varchar(100) ,
-  `range_value` double(10,2),
+  `name` varchar(45) DEFAULT NULL,
+  `username` varchar(45) NOT NULL,
+  `surname` varchar(45) DEFAULT NULL,
+  `user_type` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `birthdate` datetime DEFAULT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `phonenumber` varchar(45) DEFAULT NULL,
+  `role` varchar(50) DEFAULT 'team-member',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE sys.`criteria` (
+CREATE TABLE `range` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `issue_id` int(11) ,
-  `name` varchar(45) ,
-   `weight` double(10,2),
+  `criteria_id` int(11) DEFAULT NULL,
+  `range_name` varchar(100) DEFAULT NULL,
+  `range_value` double(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE sys.`Issue` (
+CREATE TABLE `product_details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) ,
-  `issue_name` varchar(100) ,
+  `product_id` int(11) DEFAULT NULL,
+  `criteria_id` int(11) DEFAULT NULL,
+  `range_id` int(11) DEFAULT NULL,
+  `weight` double(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-ALTER TABLE sys.Issue  ADD COLUMN status INT DEFAULT 0
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `grade` double(10,2) DEFAULT NULL,
+  `price` double(10,2) DEFAULT '0.00',
+  `selected` int(11) DEFAULT '0',
+  `issue_id` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `log` (
+  `userid` int(11) NOT NULL,
+  `operation` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Issue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `issue_name` varchar(100) DEFAULT NULL,
+  `status` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 
-SELECT  * FROM sys.Issue;
-truncate table sys.Issue;
+CREATE TABLE `criteria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `issue_id` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `weight` double(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
-SELECT  * FROM sys.criteria;
-truncate table sys.criteria;
-
-SELECT  * FROM sys.range;
-truncate table sys.range;
-
-SELECT  * FROM sys.product;
-truncate table sys.product;
-
-SELECT  * FROM sys.product_details;
-truncate table sys.product_details;
-
-
-
-# list my issues;
-select * from sys.Issue where user_id = $userid;
-
-# list supplier issues;
-select * from sys.Issue where status=1
-
-# list qualityAuthority issues;
-select * from sys.Issue where status=2
-
-
-# list productView
-select * from sys.Issue ss, sys.product pp where ss.status=0 and pp.issue_id=pp.id
